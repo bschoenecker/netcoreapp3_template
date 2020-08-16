697bedc7fe3d57829f22f598a387ae21d8f3cd47
Benjamin Schoenecker <benjamin.schoenecker@gmail.com>
2020-08-16 16:52:02 -0400

    startup.cs configuration

--- Startup.cs
+++ Startup.cs
@@ -7,13 +7,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using netcoreapp3_template.Models;
 
 namespace netcoreapp3_template
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration config) {
+            Configuration = config;
+        }
+
+        private IConfiguration Configuration {get; set;}
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
